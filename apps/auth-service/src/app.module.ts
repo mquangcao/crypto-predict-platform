b50837e7f29544196d26a6e4b77c92d0f381ac9d
Quang Cao <mquangcao38@gmail.com>
2025-12-16 23:36:47 +0700

    feat: config eslint config and prettier for auth module

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from '@app/core';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { getConfig } from '@app/common';
 
 @Module({
-  imports: [CoreModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: getConfig('core.database.type'),
+      host: getConfig('core.database.host'),
+      port: getConfig('core.database.port'),
+      username: getConfig('core.database.username'),
+      password: getConfig<string>('core.database.password'),
+      database: getConfig('core.database.dbName'),
+      synchronize: getConfig('core.database.synchronize'),
+      autoLoadEntities: true,
+    } as TypeOrmModuleOptions),
+    CoreModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
