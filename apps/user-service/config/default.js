783923d200b3376e4def3fa8a9dbbe3a9627c0a9
Quang Cao <mquangcao38@gmail.com>
2025-12-17 08:56:43 +0700

    feat: setup user service configuration option pattern

---
+++ apps/user-service/config/default.js
@@ -0,0 +1,35 @@
+module.exports = {
+  port: 4009,
+  appName: 'user-service',
+  database: {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password: 'postgres',
+    dbName: 'postgres',
+    synchronize: false,
+    logging: false,
+  },
+  core: {
+    gateway: {
+      initServices: ['user'],
+      services: {
+        auth: {
+          transport: 0,
+          options: {
+            host: 'localhost',
+            port: 8001,
+          },
+        },
+        user: {
+          transport: 0,
+          options: {
+            host: 'localhost',
+            port: 8002,
+          },
+        },
+      },
+    },
+  },
+};
